de7869fec04784b9c1dce8ba5334ebdcfc1353b7
my laptop <my laptop@admin>
2020-03-11 23:17:21 +0530

    PHPTravllers selenium basic testing

---
+++ src/test/java/com/pages/Pages.java
@@ -0,0 +1,60 @@
+package com.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Pages {
+	WebDriver driver;
+	public Pages(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		this.driver=driver;
+	}
+	
+	
+	@FindBy(xpath="//span[@class='ink animate']")
+	WebElement dropdown;
+	
+	@FindBy(xpath ="/html/body/div[2]/div[1]/div[1]/section/div/div/div[2]/div/div/div[7]/div/div[2]/div/div[3]/div/div/a")
+	WebElement readMore;
+	
+	@FindBy(xpath="//input[@placeholder='Name']")
+	WebElement name;
+	
+	@FindBy(xpath="//input[@placeholder='Phone']")
+	WebElement phone;
+	
+	@FindBy(xpath="/html[1]/body[1]/div[2]/div[1]/div[3]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/article[1]/div[3]/form[1]/fieldset[1]/div[1]/div[4]/textarea[1]")
+	WebElement message;
+	
+	
+	public void dropdown() {
+		
+		driver.findElement(By.xpath("//div[@class='dropdown dropdown-currency']//a[@id='dropdownCurrency']")).click();;
+		
+		
+		Actions a1=new Actions(driver);
+		a1.moveToElement(driver.findElement(By.xpath("//*[@id=\"header-waypoint-sticky\"]/div[1]/div/div/div[2]/div/ul/li[1]/div/div/div/a[4]"))).click().build().perform();
+	}
+	
+	public void moveToOffersPage() {
+		readMore.click();
+	}
+	
+	public void name(String userName) {
+		name.sendKeys(userName);
+	}
+	
+	public void phone(String phoneNumber) {
+		phone.sendKeys(phoneNumber);
+	}
+	
+	public void message(String messageSent) {
+		phone.sendKeys(messageSent);
+	}
+	
+
+}
---
+++ src/test/java/com/testcases/SummerVacationTest.java
@@ -0,0 +1,59 @@
+package com.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.pages.Pages;
+import com.pages.XLSLFile;
+
+public class SummerVacationTest {
+
+	WebDriver driver;
+	Pages page;
+	
+	@BeforeSuite
+	public void before_suite() {
+		//set the path of the driver
+		System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		//Getting start using of chrome browser
+		driver=new ChromeDriver();
+		//URL to be loaded on the browser
+		driver.get("https://www.phptravels.net/offers");
+		//maximize the window
+		driver.manage().window().maximize();
+		//Wait until the elemet is find. 
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		page=new Pages(driver);
+	}
+	
+	@DataProvider(name="dp")
+	public Object[][] testData(){
+		return XLSLFile.getXLSXFile("C:\\Users\\my laptop\\eclipse-workspace\\PHPTravellers\\TestData.xlsx");
+	}
+	
+	@Test(dataProvider="dp")
+	public void vaccationTest(String name,String password,String message) throws InterruptedException {
+	
+		//page.dropdown();
+		Thread.sleep(10000);
+		page.moveToOffersPage();
+		Thread.sleep(10000);
+		page.dropdown();
+		Thread.sleep(10000);
+		page.name(name);
+		String a=(String) password.subSequence(4,13);
+		page.phone(a);
+		Thread.sleep(10000);
+		page.message(message);
+		}
+}
